a8e7953175b78869b923956f32a2091fd5a791a5
Donald Pakkies <donaldpakkies@gmail.com>
2018-06-08 10:57:48 +0200

    Feat, Refactor

--- DataTypes.php
+++ DataTypes.php
@@ -4,11 +4,11 @@ namespace ModulusPHP\Framework;
 
 class DataTypes
 {
-  public static $scalar = ['boolean', 'integer', 'float', 'string'];
+  public const SCALAR = ['boolean', 'integer', 'float', 'string'];
 
-  public static $compound = ['array', 'object'];
+  public const COMPOUND = ['array', 'object'];
 
-  public static $special = ['resource', 'NULL'];
+  public const SPECIAL = ['resource', 'NULL'];
 
-  public static $all = ['boolean', 'integer', 'float', 'string', 'array', 'object', 'resource', 'NULL'];
+  public const ALL = ['boolean', 'integer', 'float', 'string', 'array', 'object', 'resource', 'NULL'];
 }
