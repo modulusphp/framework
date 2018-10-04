0e912481e2639c8e23e4a83f638f338eacc68cd2
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 12:58:22 +0200

    Feat: modulusPHP start up engine
    Boots modulusPHP, loads all the required modulus required to start the application

---
+++ Upstart.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modulus\Framework;
+
+use Modulus\Framework\Upstart\AppLogger;
+use Modulus\Framework\Upstart\AppConnect;
+use Modulus\Framework\Upstart\SwishRouter;
+use Modulus\Framework\Upstart\ErrorReport;
+use Modulus\Framework\Upstart\ViewComponent;
+use Modulus\Framework\Mocks\MustRememberMe;
+use Modulus\Framework\Upstart\UpstartThrowable;
+
+class Upstart
+{
+  use AppLogger;        // Configure application logger
+  use AppConnect;       // Initialize Database connection and Environment variables
+  use SwishRouter;      // Handle application routing
+  use ErrorReport;      // Handle application error reporting
+  use ViewComponent;    // Strap application View
+  use MustRememberMe;
+  use UpstartThrowable; // Throw exception
+
+  /**
+   * Start application
+   *
+   * @return void
+   */
+  public function boot(?bool $isConsole = false) : void
+  {
+    $this->bootEnv();
+    $this->errorHandling($isConsole);
+    $this->logger();
+
+    $this->bootRemember();
+
+    $aliases = config('app.aliases');
+
+    foreach($aliases as $alias => $class) {
+      class_alias($class, $alias);
+    }
+
+    $this->directives();
+    $this->handleSwish();
+    $this->route($isConsole);
+  }
+}
