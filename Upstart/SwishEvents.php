9a9db458121ec5df387306f4f26e418fb97af577
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 19:43:58 +0200

    Feat: assigns middleware

--- Upstart/SwishEvents.php
+++ Upstart/SwishEvents.php
@@ -2,12 +2,16 @@
 
 namespace Modulus\Framework\Upstart;
 
+use Modulus\Http\Rest;
+use Modulus\Http\Status;
 use Modulus\Http\Request;
 use Modulus\Utility\View;
 use Modulus\Utility\Events;
-use Modulus\Utility\Reflect;
 use AtlantisPHP\Swish\Route;
+use Modulus\Utility\Reflect;
 use Modulus\Utility\Middleware;
+use Modulus\Http\Exceptions\NotFoundHttpException;
+use Modulus\Framework\Exceptions\ClientErrorsException;
 
 trait SwishEvents
 {
@@ -19,7 +23,8 @@ trait SwishEvents
   private function swishFails()
   {
     return function($isAjax, $code) {
-      return Events::trigger('routes.fail', [$isAjax, $code]);
+      if ($code == 404) throw new NotFoundHttpException($isAjax, $code);
+      throw new ClientErrorsException($isAjax, $code);
     };
   }
 
@@ -33,17 +38,34 @@ trait SwishEvents
     return function($route, $callback) {
       $route->variables = (new Reflect)->handle($callback, $route->variables, $route);
 
-      foreach (Route::$routes as $key => $value) {
-        if ($value['id'] == $route->id) {
-          $route->middleware = $value['middleware'];
+      // get controller middleware
+      if (is_array($callback) && isset($callback[0]->middleware)) {
+        $method = $callback[1];
+
+        if ($callback[0]->middleware instanceof \Modulus\Http\Middleware) {
+          $middleware = $callback[0]->middleware;
+
+          if (count($middleware->only) > 0 && in_array($method, $middleware->only)) {
+            $all = $middleware->all;
+          } else if (count($middleware->except) > 0 && !in_array($method, $middleware->except)) {
+            $all = $middleware->all;
+          } else if (count($middleware->only) == 0 && count($middleware->except) == 0) {
+            $all = $middleware->all;
+          }
         }
       }
 
-      Middleware::run(
-        Reflect::$request ?? new Request(array_merge($_POST, $_FILES)),
-        $route,
-        substr($route->file, 0, strlen($route->file) - 4)
-      );
+      foreach (Route::$routes as $key => $value) {
+        if ($value['id'] == $route->id) {
+          $route->middleware = array_merge($value['middleware'], $all ?? []);
+        }
+      }
+
+      $request = Reflect::$request ?? new Request(array_merge($_POST, $_FILES));
+      $request->route = $route;
+      $this->setRequest($request);
+
+      Middleware::run($request, $route, substr($route->file, 0, strlen($route->file) - 4));
 
       return $route->variables;
     };
@@ -56,7 +78,9 @@ trait SwishEvents
    */
   private function swishAfter()
   {
-    return function($route) {
+    $events = $this;
+    return function($route) use ($events) {
+      $events->handleResponse($route->response);
       return Events::trigger('routes.handle', [$route]);
     };
   }
@@ -71,5 +95,25 @@ trait SwishEvents
     return function($path, $variables) {
       return View::make($path, isset($variables[0]) ? $variables[0] : []);
     };
+  }
+
+  /**
+   * Handle response
+   *
+   * @param mixed $response
+   * @return mixed
+   */
+  private function handleResponse($response)
+  {
+    if (is_array($response)) return Rest::response()->json($response)->send();
+
+    if (
+      is_string($response) ||
+      is_int($response) ||
+      is_float($response) ||
+      is_double($response)
+    ) return Rest::response($response)->send();
+
+    if (is_bool($response)) return Rest::response($response ? 'true' : 'false')->send();
   }
 }
