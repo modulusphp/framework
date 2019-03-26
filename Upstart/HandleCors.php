1af499577dfb953b4ef5d1e2c4d925bf8becf1eb
donald_p <donaldpakkies@gmail.com>
2019-03-26 19:10:53 +0200

    fix: Don't add Cors if this is a console application. Application might fail if this is a PHPUnit test

--- Upstart/HandleCors.php
+++ Upstart/HandleCors.php
@@ -5,24 +5,38 @@ namespace Modulus\Framework\Upstart;
 trait HandleCors
 {
   /**
-   * add Cors
+   * Add cors to request
    *
+   * @param bool $isConsole
    * @return void
    */
-  public function addCors()
+  public function addCors(bool $isConsole = false)
   {
+    /**
+     * Don't add Cors if this is a console application.
+     * Application might fail if this is a PHPUnit test
+     */
+    if ($isConsole) return;
+
+    /**
+     * This is not a console application, let's add
+     * Cors!
+     */
     header('Access-Control-Allow-Credentials: 0');
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: *');
     header('Access-Control-Allow-Methods: *');
 
+    /**
+     * Let's exit if the request type is "options"
+     */
     if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] === "OPTIONS") {
       exit;
     }
   }
 
   /**
-   * remove Cors
+   * Remove cors to request
    *
    * @return void
    */
